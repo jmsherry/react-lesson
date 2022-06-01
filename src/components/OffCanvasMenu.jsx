0069f616f020344e3bce5878977551badd65a04a
James Sherry <james.m.sherry@googlemail.com>
2022-06-01 22:37:32 +0100

    removed unused imports

--- src/components/OffCanvasMenu.jsx
+++ src/components/OffCanvasMenu.jsx
@@ -1,16 +1,16 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
+// import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import ListItemText from "@mui/material/ListItemText";
+// import InboxIcon from "@mui/icons-material/MoveToInbox";
+// import MailIcon from "@mui/icons-material/Mail";
 
 import { MenuContext } from "./../contexts/menu.context";
 
