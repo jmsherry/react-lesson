09f254313b5dad7a425113b19f505b176859ae20
James Sherry <james.m.sherry@googlemail.com>
2022-06-06 18:30:51 +0100

    added materialui textfield to form:

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -1,19 +1,41 @@
 import React, { useContext } from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import { TextField, Button } from "@mui/material";
 import { PeopleContext } from "./../contexts/person.context";
 
 export default function PersonForm() {
   let navigate = useNavigate();
   const { addPerson } = useContext(PeopleContext);
 
-  const { reset, register, handleSubmit, watch, formState: { errors } } = useForm();
+  const schema = yup.object().shape({
+    name: yup.string().required(),
+  });
+
+  const defaultValues = {
+    name: "",
+  };
+
+  const {
+    reset,
+    control,
+    // register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+    defaultValues: defaultValues,
+  });
 
   const onSubmit = (data) => {
-    console.log(data)
+    console.log(data);
     addPerson(data);
     reset();
-    navigate('/');
+    navigate("/");
   };
 
   console.log(watch("name")); // watch input value by passing the name of it
@@ -21,13 +43,18 @@ export default function PersonForm() {
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        control={control}
+        name="name"
+        render={({ field }) => (
+          <TextField error={!!errors.name} {...field} label="Name" helperText={errors.name?.message} />
+        )}
+      />
 
-
-      <input {...register("name", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.name && <span>This field is required</span>}
-      
-      <button type="submit">Submit</button>
+      <Button type="reset" onClick={reset}>
+        Reset
+      </Button>
+      <Button type="submit">Submit</Button>
     </form>
   );
 }
